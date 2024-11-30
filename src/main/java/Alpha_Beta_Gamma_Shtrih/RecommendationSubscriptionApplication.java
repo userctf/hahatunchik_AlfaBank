5fa25d456c559d97b4210e4c43d3da6e070feff8
FrogePerson <129311808+FrogePerson@users.noreply.github.com>
2024-11-30 21:50:40 +0300

    Base

--- src/main/java/Alpha_Beta_Gamma_Shtrih/RecommendationSubscriptionApplication.java
+++ src/main/java/Alpha_Beta_Gamma_Shtrih/RecommendationSubscriptionApplication.java
@@ -1,4 +1,4 @@
-package Alpha_Beta_Gamma_Shtrih.AlfaBank.Recommendation.subscription;
+package Alpha_Beta_Gamma_Shtrih;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RecommendationSubscriptionApplication {
 
 	public static void main(String[] args) {
+                System.out.print("Done!");
 		SpringApplication.run(RecommendationSubscriptionApplication.class, args);
+                System.out.print("Done!");
 	}
 
 }
