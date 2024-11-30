4facc36149075d9e4bc5a66e77c6d84088bc1417
FrogePerson <129311808+FrogePerson@users.noreply.github.com>
2024-11-30 14:59:16 +0300

    Start Web page

---
+++ src/main/java/Alpha_Beta_Gamma_Shtrih/AlfaBank/Recommendation/subscription/RecommendationSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package Alpha_Beta_Gamma_Shtrih.AlfaBank.Recommendation.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationSubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationSubscriptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Alpha_Beta_Gamma_Shtrih/AlfaBank/Recommendation/subscription/RecommendationSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package Alpha_Beta_Gamma_Shtrih.AlfaBank.Recommendation.subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationSubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
